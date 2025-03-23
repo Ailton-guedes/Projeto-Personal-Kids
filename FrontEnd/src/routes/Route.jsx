ca5c5209670418d87f3bd1af081b85907b8ca294
tchezery <tchezeryribeiro@gmail.com>
2025-03-23 16:50:08 -0300

    Criada a estrutura de pastas e estrutura FrontEnd

---
+++ FrontEnd/src/routes/Route.jsx
@@ -0,0 +1,25 @@
+{/* Precisamos disso para que ele entenda 
+    que estamos usando rotas */}
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+{/* Para incluir novas rotas precisa importar 
+    seu nome e o caminho até ela */}
+import Login from "../pages/Login";
+
+{/* Criada a constante Root para informarmos nossas rotas */}
+const Root = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/Login" replace />} />
+                
+                {/* Para incluir novas rotas, 
+                faça assim como a rota login logo abaixo: */}
+                <Route path="/Login" element={<Login />} />
+            </Routes>
+        </Router>
+    );
+};
+
+{/* Exportando para que possa ser usado no Main */}
+export default Root;
