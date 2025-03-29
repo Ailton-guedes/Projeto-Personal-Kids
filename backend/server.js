5caebc311e0ed58fb2a6e3c00ff4042cd5021157
tchezery <tchezeryribeiro@gmail.com>
2025-03-29 04:19:16 -0300

    Adicionado uma estrutura basica de backend

---
+++ backend/server.js
@@ -0,0 +1,60 @@
+//Importa as bibliotecas do express
+import express from 'express';
+
+//Cria uma constante "app" que utiliza o express. 
+// Precisa do (), pois utilizamos ele como se fosse uma função
+const app = express();
+
+//Aqui nós estamos informando que a nossa constante "app", 
+// através do express, irá receber ou enviar informações no formato json
+app.use(express.json());
+
+
+
+//Esta constante irá armazenar nossa informações, 
+// tanto para que possemos lista-las, 
+// quanto para que possamos envia-las. 
+// Ela irão ficar armazenadas dentro dos []
+const users = [];
+
+
+//Aqui estamos informando que a nossa 
+// constante "app" irá receber "post" postagen. 
+//      '/' será o caminho na url que precisa 
+//      ser utilizado para enviar uma postagem.
+//          "req" (requisitar) e o "res" (responder) são
+//           necessários para sinalizar que assim que postarmos algo,
+//           essa informação será enviada para algum lugar
+//           e que também nos dará uma resposta para sabermos
+//           que esse envio realmente foi feito
+app.post('/', (req, res) => {
+    //Aqui vemos a nossa constante "users", 
+    // que criamos para guardar as informações.
+    //      O push, indica que assim como no git push,
+    //      a nossa constante irá guardar as informações que foram enviadas.
+    //          Em seguida estamos informando para a nossa constante a onde
+    //          ela irá ver as informações que estamos enviando para que ela 
+    //          possa verificar-las e guarda-las ("req.body" -> basicamente 
+    //          dizendo: nossa requisição vira pelo body da pagina)
+    users.push(req.body);
+
+    //Aqui é estamos pedindo que após enviar a nossa requisição,
+    //  nós queremos que essa postagem nos de uma confirmação de
+    //  que realmente foi enviada a informação.
+    res.send('tch: informations was sent!');
+});
+
+
+app.get('/', (req, res) => {
+    res.json(users);
+});
+
+
+//O listen é para criar o server indicando qual
+//  a porta do nosso computador será utilizada para isso
+app.listen(3030, () => {
+   //Aqui diz que no nosso terminal,
+   //  quando o server começar a funcionar
+   //  deve apresentar a mensagem a baixo 
+   console.log('tch: server work at 3030 address');
+});
