447bd7279e79c02ebf32ecf0903d201e9db04ad4
tchezery <tchezeryribeiro@gmail.com>
2025-04-06 15:17:28 -0300

    telatela-listagem-usuarios

---
+++ frontend/src/fixed/navbar/index.jsx
@@ -0,0 +1,43 @@
+import './style.css';
+
+const Navbar = ({ children }) => {
+
+    const buttonsArray = [
+        { name: 'Dashboard', link: '/dashboard' },
+        { name: 'Agenda', link: '/agenda' },
+        { name: 'Usuarios', link: '/usuarios' },
+        { name: 'Financeiro', link: '/financeiro' },
+        { name: 'Relatorios', link: '/relatorios' },
+    ];
+
+
+    return (
+
+        <>
+            <div className="navbar">
+                <div className="buttonsArray">
+                    {buttonsArray.map((buttons, index) => (
+                        <button key={index} className="pageButton">{buttons.name}</button>
+                    ))}
+                </div>
+
+                <div className="perfil">
+                    <div className="info">
+                        <div className="role">Administrador(a),</div>
+                        <div className="name">...</div>
+                    </div>
+                    <div className="self">img</div>
+                </div>
+            </div>
+            <main>
+                {children}
+            </main>
+        </>
+
+
+
+    );
+};
+
+
+export default Navbar;
