7371f8c5cf7c41ad349f3f189823bb3664034554
ailton guedes <ailton2218@gmail.com>
2025-04-06 15:40:53 -0300

    ajuste

--- FrontEnd/src/pages/administrator/CadastroDeAluno/index.jsx
+++ FrontEnd/src/pages/administrator/CadastroDeAluno/index.jsx
@@ -1,4 +1,3 @@
-
 import './style.css';
 
 const CadastroDeAluno = () => {
@@ -10,24 +9,49 @@ const CadastroDeAluno = () => {
                 <input type="text" id="nome" name="nome" required />
                 <br /><br />
 
-                <label htmlFor="data_nascimento">Data de Nascimento</label>
-                <input type="date" id="data_nascimento" name="data_nascimento" required />
+                <label htmlFor="cpf_aluno">CPF do Aluno</label>
+                <input type="text" id="cpf_aluno" name="cpf_aluno" required />
+                <br /><br />
+
+                <div className="half-fields">
+                    <div>
+                        <label htmlFor="data_nascimento">Data de Nascimento</label>
+                        <input type="date" id="data_nascimento" name="data_nascimento" required />
+                    </div>
+
+                    <div>
+                        <label htmlFor="cep">CEP</label>
+                        <input type="text" id="cep" name="cep" required />
+                    </div>
+                </div>
+
                 <br /><br />
 
                 <label htmlFor="nome_responsavel">Nome do Responsável</label>
                 <input type="text" id="nome_responsavel" name="nome_responsavel" required />
                 <br /><br />
 
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" required />
+                <div className="half-fields">
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" required />
+                    </div>
+
+                    <div>
+                        <label htmlFor="telefone">Telefone</label>
+                        <input type="text" id="telefone" name="telefone" required />
+                    </div>
+
+                    <div>
+                        <label htmlFor="cpf_responsavel">CPF do Responsável</label>
+                        <input type="text" id="cpf_responsavel" name="cpf_responsavel" required />
+                    </div>
+                </div>
+
                 <br /><br />
 
                 <label htmlFor="endereco">Endereço</label>
                 <input type="text" id="endereco" name="endereco" required />
-                <br /><br />
-
-                <label htmlFor="telefone">Telefone</label>
-                <input type="text" id="telefone" name="telefone" required />
                 <br /><br />
 
                 <label htmlFor="plano">Plano</label>
