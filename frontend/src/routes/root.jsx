8ba3392fed747e2d350e062333403ff487d94e65
ailton guedes <ailton2218@gmail.com>
2025-05-13 11:12:37 -0300

    tela cadastro de aluno entegração com beck

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -11,7 +11,9 @@ import Agenda from "../pages/administrator/agenda";
 import User from "../pages/administrator/usuarios";
 import Cadastro_responsavel from "../pages/administrator/usuarios/cadastro_responsavel";
 import Cadastro_professor from "../pages/administrator/usuarios/cadastro_professor";
+import CadastroAluno from "../pages/administrator/usuarios/cadastro_aluno";
 import Perfil from "../pages/administrator/usuarios/perfil";
+
 
 import Financeiro from "../pages/administrator/financeiro";
 import Relatorios from "../pages/administrator/relatorio";
@@ -30,8 +32,9 @@ const Root = () => {
                 <Route path="/agenda" element={<Navbar><Agenda /></Navbar>} /> 
 
                 <Route path="/usuarios" element={<Navbar><User /></Navbar>} /> 
-                <Route path="/usuarios/cadastro-de-aluno" element={<Navbar><Cadastro_responsavel /></Navbar>} />
+                <Route path="/usuarios/cadastro-de-responsavel" element={<Navbar><Cadastro_responsavel /></Navbar>} />
                 <Route path="/usuarios/cadastro-de-professor" element={<Navbar><Cadastro_professor /></Navbar>} />
+                <Route path="/usuarios/cadastro-de-aluno" element={<Navbar><CadastroAluno /></Navbar>} />
                 <Route path="/usuarios/perfil/:id" element={<Navbar><Perfil /></Navbar>} />
 
                 <Route path="/financeiro" element={<Navbar><Relatorios /></Navbar>} />
